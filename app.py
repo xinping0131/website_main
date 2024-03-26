e628a1c4e22f8113a364f32b550ec8415ca833bd
xinping0131 <150782519+xinping0131@users.noreply.github.com>
2024-03-26 22:01:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from application.application import app
+import openai
+import os
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
+@app.route('/generate',method=['POST'])
+def generate():
+    prompt=request.form['promp']
+    response = openai.ChatCompletion.create(
+        messages=[
+            {"role":"user","content":prompt}
+        ],
+        model="gpt-3.5-turbo-0125",
+        temperature=0.5,
+    )
+    generated_text = response['choices'][0]['message']['content'].strip()
+    return render_template('index.html',response = generated_text)
+    
 if __name__ == "__main__":
     app.run(debug=True)
